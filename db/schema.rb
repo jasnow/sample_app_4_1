d6a079e2006fa4919700f654cd019a487beb9e3b
Al Snow <jasnow@hotmail.com>
2014-12-30 12:50:10 -0500

    Update schema.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20110521134254) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "microposts", force: true do |t|
     t.string   "content"
     t.integer  "user_id"
@@ -23,7 +20,7 @@ ActiveRecord::Schema.define(version: 20110521134254) do
     t.datetime "updated_at"
   end
 
-  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id", using: :btree
+  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
   create_table "relationships", force: true do |t|
     t.integer  "follower_id"
@@ -32,8 +29,8 @@ ActiveRecord::Schema.define(version: 20110521134254) do
     t.datetime "updated_at"
   end
 
-  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
-  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -45,6 +42,6 @@ ActiveRecord::Schema.define(version: 20110521134254) do
     t.boolean  "admin",              default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
