9f19b97349f1895ecee11d62ec2810e490cfb791
Al Snow <jasnow@hotmail.com>
2017-10-20 18:46:31 -0400

    Upgraded from FactoryGirl to FactoryBot

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -39,7 +39,7 @@ describe SessionsController, :type => :controller do
 
     describe "success" do
       before(:each) do
-        @user = FactoryGirl.create(:user)
+        @user = FactoryBot.create(:user)
         @attr = { :email => @user.email, :password => @user.password }
       end
 
@@ -58,7 +58,7 @@ describe SessionsController, :type => :controller do
 
   describe "DELETE 'destroy'" do
     it "should sign a user out" do
-      test_sign_in(FactoryGirl.create(:user))
+      test_sign_in(FactoryBot.create(:user))
       delete :destroy
       expect(controller).not_to be_signed_in
       expect(response).to redirect_to(root_path)
